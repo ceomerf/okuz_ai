8ead7765898e8f526267e8ac0a7a935cc27971dd
ceomerf <ceomerf@gmail.com>
2025-10-06 21:27:01 +0300

    werwer

--- backend/src/realtime/realtime.module.ts
+++ backend/src/realtime/realtime.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { RealtimeGateway } from './realtime.gateway';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<string>('JWT_ACCESS_TOKEN_EXPIRATION') || '1h' },
+      }),
+    }),
+  ],
   providers: [RealtimeGateway],
   exports: [RealtimeGateway],
 })
