afc85de47965eef25220435bae126cc697597342
ceomerf <ceomerf@gmail.com>
2025-10-08 02:55:23 +0300

    sdfs

--- backend/src/common/cache/cache.module.ts
+++ backend/src/common/cache/cache.module.ts
@@ -1,12 +1,19 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
 import { CacheService } from './cache.service';
 import { CacheInterceptor } from './cache.interceptor';
 import { CacheEvictInterceptor } from './cache-evict.interceptor';
 
 @Global()
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    NestCacheModule.register({
+      ttl: 300, // 5 minutes
+      max: 100, // maximum number of items in cache
+    }),
+  ],
   providers: [CacheService, CacheInterceptor, CacheEvictInterceptor],
   exports: [CacheService, CacheInterceptor, CacheEvictInterceptor],
 })
