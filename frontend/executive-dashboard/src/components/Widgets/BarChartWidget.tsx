0c1b2801731f310948ce5e82fa5943371ecfa4d5
ceomerf <ceomerf@gmail.com>
2025-10-09 22:31:49 +0300

    komik

--- frontend/executive-dashboard/src/components/Widgets/BarChartWidget.tsx
+++ frontend/executive-dashboard/src/components/Widgets/BarChartWidget.tsx
@@ -9,6 +9,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+import { Box, Typography } from '@mui/material';
 import BaseWidget from './BaseWidget';
 
 // Chart.js bileşenlerini kaydet
